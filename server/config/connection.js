5d6013b4553fcce579272575f83e35a625add7b6
JVelezFD <jvelez117@gmail.com>
2022-10-03 17:52:04 -0400

    updated connectiong URI and env requirements for Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.ATLAS_URL,
+  process.env.MONGODB_URI,
   // || 'mongodb://127.0.0.1:27017/triviyaa',
   {
     useNewUrlParser: true,
