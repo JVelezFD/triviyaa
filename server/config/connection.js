777f86e6d057f019a8d1020c7ee7d7f82ab545ad
Jared Daniel <60400956+jareddaniel95@users.noreply.github.com>
2022-10-05 20:49:33 -0400

    Make frontend work

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.ATLAS_URL,
+  "mongodb+srv://triviyaa:YahooThe3estEmail@cluster0.ewe2sqe.mongodb.net/triviyaaDB?retryWrites=true&w=majority",
   // || 'mongodb://127.0.0.1:27017/triviyaa',
   {
     useNewUrlParser: true,
