fd8388d9b3b90622012b9adb9badf3a2c2d36ba6
JVelezFD <jvelez117@gmail.com>
2022-10-15 11:46:52 -0400

    we got the path to images working

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,13 @@
 
 import { useState } from 'react';
 import '../index.css';
-import { QuickPlayPic } from '../../src/QuickPlayPic.png'; // Tell Webpack this JS file uses this image
+import quickplay from '../ourpics/QuickPlayPic.png';
+import logninpic from '../ourpics/LogInPic.png';
+import howtoplay from '../ourpics/HowToPlayPic.png';
+ 
+ 
+ 
+ // Tell Webpack this JS file uses this image
 
 function Landing() {
   const [size] = useState(600);
@@ -9,16 +15,16 @@ function Landing() {
   
   return (
     <div className="Landing">
-      <h1>Welcome to Trivyaa : The biggest innovation in trivia since breweries became cool</h1>
+      <h1 className= "text-primary">Welcome to Trivyaa : The biggest innovation in trivia since breweries became cool</h1>
 
       <div className="Container">
         <div className="Row1">
           <div className="media">
-            <img className="card1 col-sm" src={ QuickPlayPic } alt="Quick Play"></img>
+            <img className="card1 col-sm" src={ quickplay } alt="Quick Play"></img>
           </div>
-          <div className="card2 col-sm" src="../../client/public/LogInPic.png"></div>
+          {/* <div className="card2 col-sm" src="../../client/public/LogInPic.png"></div>
+        </div> */}
         </div>
-        
         {/* <div className="Row2">
           <div className="card3" class="col-sm" src="client/public/HowToPlayPic.png"></div>
           <div className="card4" class="col-sm" src="client/public/AboutUsPic.png"></div>
