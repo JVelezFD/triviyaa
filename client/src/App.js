90ffac3d61054d46c8e50252965eae2cde106543
JVelezFD <jvelez117@gmail.com>
2022-10-18 21:58:47 -0400

    added layout and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -6,9 +6,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "./components/Loading";
 import ProtectedRoute from "./auth/protected-route";
 
-import { Room, QR, CreateRoom, Landing, Profile } from "./pages";
+import { Room, QR, Landing, } from "./pages";
 
-import { Header } from './components';
+import { GameContainer, SignupButton, LoginButton, LogoutButton, Header, AuthNav, AuthButton,LoginUser, Loading, DisplayGameOver,DisplayUsers   } from './components';
 
 // import CreateRoom from './pages/CreateRoom';
 
@@ -18,20 +18,64 @@ import About from './pages/About.js';
 import HowTo from './pages/HowTo.js';
 
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
 
-function App() {
-  const { isLoading } = useAuth0();
+function App({ socket }) {
+  const [user, setUser] = useState("")
+  const [room, setRoom] = useState("")
+  const [admin, setAdmin] = useState(false)
 
-  if (isLoading) {
-    return <Loading />;
+  const [error, setError] = useState(null)
+  const [userObject, setUserObject] = useState({})
+
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [question, setQuestion] = useState("")
+  const [usersInGame, setUsersInGame] = useState([])
+
+  const [gameStarted, setGameStarted] = useState(false)
+  const [endOfGame, setEndOfGame] = useState(false)
+
+  const handleStart = () => {
+    setGameStarted(true)
+    setEndOfGame(false)
+    socket.emit("start")
   }
 
+  useEffect(() => {
+    // global emits to server socket.io
+    socket.on("error", () => {
+      setUser("")
+      setRoom("")
+      setLoggedIn(false)
+      setError("username already claimed!")
+    })
+
+    socket.on("userInfo", (user) => {
+      setUserObject(user)
+    })
+
+    socket.on("gameStartedInRoom", () => {
+      setEndOfGame(false)
+      setGameStarted(true)
+    })
+    socket.on("timedQuestion", (data) => {
+      setQuestion(data)
+    })
+
+    socket.on("message", (data) => {
+      console.log(data)
+    })
+    socket.on("roomData", (roomData) => {
+      setUsersInGame(roomData.users)
+    })
+
+    socket.on("endOfGame", () => {
+      setEndOfGame(true)
+      setGameStarted(false)
+    })
+  }, [socket])
+
   return (
-    <ApolloProvider client={client}>
+    
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
@@ -71,7 +115,7 @@ function App() {
           </div>
         </div>
       </Router>
-    </ApolloProvider>
+  
   );
 };
 
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,9 +4,14 @@ import Header from "./Header";
 import Loading from "./Loading";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
-import RoomForm from "./RoomForm";
+import GameContainer from "./GameContainer";
 import SignupButton from "./SignupButton";
+import LoginUser from "./LoginInUser";
+import DisplayUsers from "./DisplayUsers";
+import DisplayGameOver from "./DisplayGameOver";
 
 
 
-export { RoomForm, SignupButton, LoginButton, LogoutButton, Header, AuthNav, AuthButton, Loading };
+
+
+export { GameContainer, SignupButton, LoginButton, LogoutButton, Header, AuthNav, AuthButton,LoginUser, Loading, DisplayGameOver,DisplayUsers };
